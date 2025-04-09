ebd3840bfc6da3a36b9abef232e56850530b3d98
Thy_Ghy <89333336+ThyGhy@users.noreply.github.com>
2025-04-09 08:21:08 -0700

    Shell Updates

--- update.sh
+++ update.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-echo "Running update script at $(date)" >> /app/webhook.log
-pwd >> /app/webhook.log
-ls -la >> /app/webhook.log
-
 cd /app
-echo "Pulling latest changes from GitHub..."
-git pull origin main
-echo "Update complete."
-
-echo "Done pulling. Restarting Flask app..."
-touch app.py
+git pull
