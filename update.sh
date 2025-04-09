600fc841ee9c83041473bcd0c0d87ff0715d46a8
Thy_Ghy <89333336+ThyGhy@users.noreply.github.com>
2025-04-09 09:08:21 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Pulling latest changes from GitHub..."
+echo "Running update script at $(date)"
 cd /app
+
+echo "Resetting local changes..."
+git reset --hard HEAD
+
+echo "Pulling latest changes from GitHub..."
 git pull
+
 echo "Update complete."
 echo "Done pulling. Restarting Flask app..."
