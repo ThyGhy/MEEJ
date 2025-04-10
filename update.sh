dc689b58ad5ef256618011afce0cb6067b062803
Thy_Ghy <89333336+ThyGhy@users.noreply.github.com>
2025-04-10 12:04:35 -0700

    Shell Updates

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running update script at $(date)"
-cd /app
+cd /mnt/StoragePool/Github/MEEJ
 
 echo "Resetting local changes..."
 git reset --hard HEAD
@@ -10,4 +10,7 @@ echo "Pulling latest changes from GitHub..."
 git pull
 
 echo "Update complete."
-echo "Done pulling. Restarting Flask app..."
+
+echo "Rebuilding and restarting Docker container..."
+docker compose down
+docker compose up -d --build
