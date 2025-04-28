5c06d1f4fe2afa5ac54ed7d7e9bfd5f1a421ce54
chunklet <bellabartholomew199@gmail.com>
2025-04-28 16:32:06 -0700

    stuffs
    Added flash to app.py for popups once i figure those out. Everything else is mostly formatting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, flash, render_template, request, redirect, url_for, session
 import database  # This is our custom module
 import re
 import datetime
